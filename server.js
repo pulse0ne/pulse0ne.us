92f7d8c279fc57efa83cf032e3cf28c9d1d83529
Tyler Snedigar <tylereatsairplane@gmail.com>
2017-03-02 05:06:25 +0000

    Implement http --> https redirect

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ var a = e();
 var s = require('https');
 var f = require('fs');
 
-a.use(e.static(p.join(__dirname, 'pub')));
+var http_port = 80;
+var https_port = 443;
+
+a.use(e.static(p.join(__dirname, 'public')));
 
 //a.listen(80);
 var o = {
-  key: f.readFileSync('/etc/letsencrypt/live/pulse0ne.us/privkey.pem'),
-  cert: f.readFileSync('/etc/letsencrypt/live/pulse0ne.us/fullchain.pem')
+  key: f.readFileSync(p.join(__dirname, 'certs/privkey.pem')),
+  cert: f.readFileSync(p.join(__dirname, 'certs/fullchain.pem'))
 };
-s.createServer(o, a).listen(443);
+s.createServer(o, a).listen(https_port);
+
+var http = e();
+
+http.get('*', function (req, res) {
+	res.redirect('https://www.pulse0ne.us' + req.url)
+});
+
+http.listen(http_port);
