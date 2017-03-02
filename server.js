12f136540cb8de6fb799742604a691540b12e77f
Tyler Snedigar <snedigart@gmail.com>
2017-03-02 00:08:14 -0600

    updates

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-var p = require('path');
-var e = require('express');
-var a = e();
-var s = require('https');
-var f = require('fs');
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var https = require('https');
+var fs = require('fs');
+
+var app = express();
 
 var http_port = 80;
 var https_port = 443;
 
-a.use(e.static(p.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//a.listen(80);
-var o = {
-  key: f.readFileSync(p.join(__dirname, 'certs/privkey.pem')),
-  cert: f.readFileSync(p.join(__dirname, 'certs/fullchain.pem'))
+var https_options = {
+    key: fs.readFileSync(path.join(__dirname, 'certs/privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'certs/fullchain.pem'))
 };
-s.createServer(o, a).listen(https_port);
+https.createServer(https_options, app).listen(https_port);
 
-var http = e();
 
+// Redirect all http traffic to https
+var http = express();
 http.get('*', function (req, res) {
-	res.redirect('https://www.pulse0ne.us' + req.url)
+    res.redirect('https://' + req.headers.host + req.url);
 });
-
 http.listen(http_port);
