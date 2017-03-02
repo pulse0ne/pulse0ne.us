a9225c4a7cae681f5b054a44c9736b01cf616780
Tyler Snedigar <Tyler.D.Snedigar@raytheon.com>
2017-03-02 12:10:44 -0600

    Provide for optional configuration

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var app = express();
 var http_port = 80;
 var https_port = 443;
 
+try {
+    var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+    http_port = config.http_port;
+    https_port = config.https_port;
+} catch (e) {
+    // no config
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var https_options = {
